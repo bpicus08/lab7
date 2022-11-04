d0e9ed0efcac79efb6fd3edbc48b55af178515cc
bpicus08 <bpicus@hawk.iit.edu>
2022-11-04 15:18:27 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 function init(){
 	//add javascript between these two lines of code
-	
-	
-	
-	
-	
-	
-	
+	function nameAlert(){
+		var text = document.getElementByID('entryinput').value;
+		var h2result = document.getElementById('textoutput');
+		h2result.innerHTML = text;
+		alert("Brett Picus: "+text);
+	}
+	var divButton = document.getElementById('entrybutton');
+	divButton.addEventListener('click',nameAlert);
 }
 window.addEventListener('load', init);
