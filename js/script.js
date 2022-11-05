c2d5ccc0beb078934c4f4b858abb04a230acbc4f
bpicus08 <bpicus@hawk.iit.edu>
2022-11-04 20:18:38 -0500

    Fixed Cascade which made program actually work

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 function init(){
-	//add javascript between these two lines of code
-	function nameAlert(){
-		var text = document.getElementByID('entryinput').value;
-		var h2result = document.getElementById('textoutput');
-		h2result.innerHTML = text;
-		alert("Brett Picus: "+text);
-	}
-	var divButton = document.getElementById('entrybutton');
-	divButton.addEventListener('click',nameAlert);
+//add your javascrip between these two lines of code
+  function nameAlert(){
+    var text = document.getElementById('entryinput').value;
+    var h2result = document.getElementById('textoutput');
+    h2result.innerHTML = text;
+    alert("Brett Picus: "+text);
+  }
+  var divButton = document.getElementById('entrybutton');
+  divButton.addEventListener('click',nameAlert);
 }
 window.addEventListener('load', init);
